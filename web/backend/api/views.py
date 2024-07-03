35bf665ccaf0b4d0d60e34418bece1adebb7fbe5
anneli <annelihu@web.de>
2024-07-03 18:28:18 +0200

    added upload for patients csv (doesn't work yet)

--- web/backend/api/views.py
+++ web/backend/api/views.py
@@ -10,6 +10,10 @@ import os
 import numpy as np
 from xgboost import XGBClassifier
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.parsers import  MultiPartParser
+from rest_framework import status
+import csv
+from io import StringIO
 
 model_path = "models/model_full"
 loaded_model = XGBClassifier()
@@ -98,3 +102,38 @@ class PredictView(APIView):
         prediction = loaded_model.predict([values])
         prediction = float(prediction[0])
         return Response({'prediction': prediction})
+
+class PatientCSVUploadView(APIView):
+    parser_classes = [MultiPartParser]
+    permission_classes = [IsAuthenticated]  
+
+    def post(self, request, format=None):
+        file_obj = request.FILES.get('file')
+        if not file_obj:
+            return Response({"error": "No file provided"}, status=400)
+
+        if not file_obj.name.endswith('.csv'):
+            return Response({"error": "Invalid file format. Please upload a CSV file."}, status=400)
+
+        user = request.user 
+
+        try:
+            decoded_file = StringIO(file_obj.read().decode('utf-8'))
+            reader = csv.DictReader(decoded_file)
+            for row in reader:
+                Patient.objects.create(
+                    user=user,  
+                    nachname=row['Nachname'],
+                    vorname=row['Vorname'],
+                    geschlecht=row['Geschlecht'],
+                    geburtsdatum=row['Geburtsdatum'],
+                    aufnahmedatum=row['Aufnahmedatum'],
+                    id_nr=row['ID-Nr'],
+                    aki_score=row['AKI-Score'],
+                    diagnose=row['Diagnose']
+                )
+            return Response({"success": "Patients added successfully."}, status=201)
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
+
+
