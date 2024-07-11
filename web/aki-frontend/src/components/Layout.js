e350aa91f77becd2af032f8ff4516494b13865ba
anneli <annelihu@web.de>
2024-07-11 14:46:44 +0200

    design changes

--- web/aki-frontend/src/components/Layout.js
+++ web/aki-frontend/src/components/Layout.js
@@ -4,9 +4,9 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children, user, onLogout }) => {
   return (
-    <Box sx={{ display: 'flex', height: '100vh', bgcolor: '#ffffff' }}> {/* Set background color to white */}
+    <Box sx={{ display: 'flex', height: '100vh', width: '100vw', bgcolor: '#ffffff' }}> 
       <Sidebar user={user} onLogout={onLogout} />
-      <Container sx={{ flexGrow: 1, p: 3, display: 'flex', flexDirection: 'column', bgcolor: '#ffffff' }}>
+      <Container sx={{ flexGrow: 1, p: 3, display: 'flex', flexDirection: 'column', bgcolor: '#ffffff', height: '100%' }}>
         {children}
       </Container>
     </Box>
