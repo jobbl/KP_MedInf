5643bbccbeb31552f84dc97b3e09fa38c6ea40d5
Jordan <jordan.wenzel@nibiru-systems.com>
2024-07-16 15:36:01 +0200

    increase login token duration

--- web/backend/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Patient, PatientFeature, PatientPrediction
-
-admin.site.register(Patient)
-admin.site.register(PatientFeature)
-admin.site.register(PatientPrediction)
